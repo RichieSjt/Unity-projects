8d769fd72d15489464c5bf47bbb67e6307fb7e63
Richie_Sjt <richi252012@hotmail.com>
2020-05-15 23:18:20 -0500

    Diamonds

---
+++ Unity Projects/Homework 8/Assets/Scripts/PlayerCollisionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionController : MonoBehaviour{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Item"))
+        {
+            Destroy(other.gameObject);
+            WinManager.instance.diamondsCounter += 1;
+            Debug.Log("Picking up diamond");
+            Debug.Log(WinManager.instance.diamondsCounter);
+            WinManager.instance.CheckWin();
+        }
+        if(other.gameObject.CompareTag("Water") || other.gameObject.CompareTag("Enemy"))
+        {
+            transform.position=new Vector3(-3, 2, 6);
+        }
+    }
+
+}
---
+++ Unity Projects/Homework 8/Assets/Scripts/WinManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinManager : MonoBehaviour{
+
+    public bool playerWon;
+    public int diamondsCounter;
+    
+    public static WinManager instance; 
+
+    private void Awake()
+    {
+        instance = this;
+        playerWon = false;
+        diamondsCounter = 0;
+    }
+
+    public void CheckWin()
+    {
+        Debug.Log(diamondsCounter);
+        if(diamondsCounter == 3)
+            playerWon = true;
+    }
+}
